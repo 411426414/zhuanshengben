02292b8d7b3860bb8374041f4fc277da22e41962
411426414 <411426414@qq.com>
2021-05-30 20:51:54 +0800

    添加数据库配置(dev)

---
+++ server/config.js
@@ -0,0 +1,14 @@
+// 默认dev配置
+const config = {
+    DEBUG: false,// 是否调试
+    // MYSQL数据库连接配置
+    MYSQL:{
+        host: 'localhost',
+        database: 'zsb.e21.cn',
+        username:'root',
+        password: ''
+    },
+    host: 'localhost',
+    port: 4000
+}
+module.exports = config
